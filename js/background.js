b91f61046e65fd99adad382b2be3ab5deae90910
Andy Hawkes <andy@andyhawkes.com>
2019-07-11 13:58:03 -0700

    Fix race condition.
    Two functions, finishRequest and headersReceived, both delegated to async requestUpdated, but they weren't waiting on the promise to finish. This created a race condition where requests were being considered "completed" before all the headers had been copied over, and sent to the server without headers.
    
    This change fixes those race conditions, but I'm still a bit concerned about the possibility of race conditions from the primary listeners finishing out of order. For example, is it possible that the handler for onCompleted might execute before the handler for onResponseStarted was done? I haven't been able to make that happen so far but it's something to keep in mind.

--- js/background.js
+++ js/background.js
@@ -68,10 +68,10 @@ async function requestUpdated (info) {
 //
 // Updates a request and checks if it's being redirected.
 // 
-function headersReceived (info) {
-    requestUpdated(info);
+async function headersReceived (info) {
+    await requestUpdated(info)
 
-    if (info.statusCode == 301 || info.statusCode == 302) {
+    if (info.statusCode === 301 || info.statusCode === 302) {
         requestRedirected(info);
     }
 };
@@ -105,11 +105,11 @@ function requestRedirected (info) {
 //
 // Finishes a normal request, marking it completed.
 //
-function finishRequest (info) {
+async function finishRequest (info) {
+    await requestUpdated(info);
+
     info.completed = true;
     info.timeCompleted = new Date().getTime();
-
-    requestUpdated(info);
 };
 
 //
@@ -240,7 +240,7 @@ browser.tabs.onActivated.addListener(async function (activeInfo) {
             });
         } catch (err) {
             console.log(err.message); // Could not establish connection. Receiving end does not exist.
-            
+
             manifest.content_scripts.forEach(data => {
                 data.js.forEach(script => {
                     browser.tabs.executeScript(tabId, {file: script});
